dc81cfadbe86e809f1a6cdc0e0aea0b35702a1a0
Lam K. <immane@users.noreply.github.com>
2026-05-14 19:10:55 +0800

    chore(service): include pending ContentService changes

--- src/Common/Service/ContentService.php
+++ src/Common/Service/ContentService.php
@@ -4,10 +4,11 @@ namespace App\Common\Service;
 
 use App\Common\Entity\Content;
 use App\Core\Service\BaseService;
+use App\Core\Service\BaseServiceInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use App\Core\Service\ServiceLocatorInterface;
 
-class ContentService extends BaseService
+class ContentService extends BaseService implements BaseServiceInterface
 {
     public function __construct(
         ContainerInterface $container,
