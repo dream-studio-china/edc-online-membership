4b0fb3db12b8a88ad90970de8082ca084bb277df
Lam K. <immane@users.noreply.github.com>
2026-07-03 11:49:27 +0800

    fix(setting): quote reserved-word columns key/value in Setting entity
    - Add loadClassMetadata event listener to mark Setting key/value as quoted
    - MySQL reserved words not quoted by default in ORM Column attribute
    - Add unit test for quoted metadata and integration test for Setting CRUD

--- src/Core/EventListener/ControllerListener.php
+++ src/Core/EventListener/ControllerListener.php
@@ -2,12 +2,15 @@
 
 namespace App\Core\EventListener;
 
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Events;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
-
+#[AsDoctrineListener(Events::loadClassMetadata)]
 class ControllerListener
 {
     /** @var ContainerInterface */
@@ -16,6 +19,10 @@ class ControllerListener
     private $tokenStorage;
     /** @var LoggerInterface */
     private $logger;
+
+    private const QUOTED_COLUMNS = [
+        'App\\Common\\Entity\\Setting' => ['key', 'value'],
+    ];
 
     public function __construct(ContainerInterface $container, TokenStorageInterface $tokenStorage, LoggerInterface $logger)
     {
@@ -50,6 +57,22 @@ class ControllerListener
             $this->logger->info(
                 "User [#$operator] Requests $method $uri: $content"
             );
+        }
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $args): void
+    {
+        $classMetadata = $args->getClassMetadata();
+        $className = $classMetadata->getName();
+
+        if (!isset(self::QUOTED_COLUMNS[$className])) {
+            return;
+        }
+
+        foreach (self::QUOTED_COLUMNS[$className] as $fieldName) {
+            if (isset($classMetadata->fieldMappings[$fieldName])) {
+                $classMetadata->fieldMappings[$fieldName]->quoted = true;
+            }
         }
     }
 }
